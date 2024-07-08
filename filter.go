d3e413a85f059953d858e902276ae280fa40f8ea
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2024-07-08 15:22:25 -0600

    filter, transform, and reduce all compile

---
+++ filter.go
@@ -0,0 +1,41 @@
+package iterh
+
+import (
+	"iter"
+)
+
+func Filter[T any](it iter.Seq[T], filt func(T) bool) iter.Seq[T] {
+	return func(y func(T) bool) {
+		for val := range it {
+			if filt(val) {
+				if ok := y(val); !ok {
+					return
+				}
+			}
+		}
+	}
+}
+
+func Transform[T, U any](it iter.Seq[T], trans func(T) U) iter.Seq[U] {
+	return func(y func(U) bool) {
+		for val := range it {
+			if ok := y(trans(val)); !ok {
+				return
+			}
+		}
+	}
+}
+
+func Reduce[T any](it iter.Seq[T], red func(T, T) T) T {
+	var sum T
+	i := 0
+	for val := range it {
+		if i == 0 {
+			sum = val
+		} else {
+			sum = red(sum, val)
+		}
+		i++
+	}
+	return sum
+}
