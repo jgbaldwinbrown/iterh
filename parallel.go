dedc4da5ded0e65ccf7c481d400e6781e76c55b9
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2024-09-13 11:01:54 -0600

    added parallelrunerr

--- parallel.go
+++ parallel.go
@@ -5,7 +5,21 @@ import (
 	"iter"
 )
 
-func ParallelRun(fs iter.Seq[func() error], threads int) error {
+func ParallelRun(fs iter.Seq[func()], threads int) {
+	var g errgroup.Group
+	if threads > 0 {
+		g.SetLimit(threads)
+	}
+	for f := range fs {
+		g.Go(func() error {
+			f()
+			return nil
+		}
+	}
+	g.Wait()
+}
+
+func ParallelRunErr(fs iter.Seq[func() error], threads int) error {
 	var g errgroup.Group
 	if threads > 0 {
 		g.SetLimit(threads)
